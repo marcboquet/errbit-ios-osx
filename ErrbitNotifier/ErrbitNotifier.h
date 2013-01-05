f35176d7a57d166ccfa45a38f6f9c303026c74a1
Richard Hancock <rjhancock@me.com>
2013-01-04 20:44:28 -0600

    Added old code back in under new localtion
    Signed-off-by: Richard Hancock <rjhancock@me.com>

---
+++ ErrbitNotifier/ErrbitNotifier.h
@@ -0,0 +1,13 @@
+//
+//  ErrbitNotifier.h
+//  ErrbitNotifier
+//
+//  Created by Richard Hancock on 1/4/13.
+//  Copyright (c) 2013 Richard Hancock. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ErrbitNotifier : NSObject
+
+@end
